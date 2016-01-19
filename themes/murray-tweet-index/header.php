1a5218e31ad9b003ff2e6f76389c568cff0f6b7e
Peter Barr <peter.barr@murraygroup.ie>
2016-01-19 15:29:30 +0000

    Murray Logo added

--- themes/murray-tweet-index/header.php
+++ themes/murray-tweet-index/header.php
@@ -6,13 +6,15 @@
         <title>Murray Tweet Index</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <script src="https://use.typekit.net/yha1wfq.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
 		<link href="//www.google-analytics.com" rel="dns-prefetch">
         <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/touch.png" rel="apple-touch-icon-precomposed">
 
         <?php wp_head(); ?>
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+        
         
     </head>
     <body <?php body_class(); ?>>
@@ -21,5 +23,6 @@
 
             <header>
                 
-                
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/images/murray-logo.svg">
+
             </header>
