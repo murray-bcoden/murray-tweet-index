70b9d47e886e1268f6fb825f6aaa569e99e53cb5
Peter Barr <peter.barr@murraygroup.ie>
2016-01-19 14:46:45 +0000

    File admin

---
+++ themes/murray-tweet-index/header.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html <?php language_attributes(); ?> class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Murray Tweet Index</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link href="//www.google-analytics.com" rel="dns-prefetch">
+        <link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon">
+        <link href="<?php echo get_template_directory_uri(); ?>/touch.png" rel="apple-touch-icon-precomposed">
+
+        <?php wp_head(); ?>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/main.css">
+        
+    </head>
+    <body <?php body_class(); ?>>
+
+        <div class="wrapper">
+
+            <header>
+                
+                
+            </header>
