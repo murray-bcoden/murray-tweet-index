ebeba61a731bc964aad42a4774480acc7f6eaa05
Peter Barr <peter.barr@murraygroup.ie>
2016-01-21 11:45:16 +0000

    Sections setup

--- themes/murray-tweet-index/includes/parameters.php
+++ themes/murray-tweet-index/includes/parameters.php
@@ -1,8 +1,8 @@
-\<section class="section params">
+<section class="section params">
 
 	<div class="params-wrapper">
-		<h2 class="section-title">Category Winners</h2>
-		<p class="section-intro">In addition to the overall rankings, the research ranked journalists’ influence on Twitter in a range of different categories, category winners included:</p>
+		<h2 class="section-title">6 Parameters</h2>
+		<p class="section-intro">Top ranking journalists across each of the 6 parameters measured:</p>
 	</div>
 	
 </section>
