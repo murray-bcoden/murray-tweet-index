ab75da22ac2d6a96e0d70b981e2f702236697fd1
Peter Barr <peter.barr@murraygroup.ie>
2016-01-21 18:21:09 +0000

    Gains init

--- themes/murray-tweet-index/includes/biggest-gains.php
+++ themes/murray-tweet-index/includes/biggest-gains.php
@@ -7,24 +7,24 @@
 			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/gains.svg" alt="Positions gained infographic" class="gains-info">
 			<ol class="gains-people">
 				<li class="gains-people-item">
-					<h3 class="gains-people-name"><span>Carol Hunt</span> @carolmhunt</h3>
-					<p class="gains-people-title">Columnist, Sunday Independent</p>
+					<h3 class="gain-title"><span class="gain-number">218</span> <span class="gain-name">Carol Hunt</span> @carolmhunt</h3>
+					<p class="gain-job">Columnist, Sunday Independent</p>
 				</li>
 				<li class="gains-people-item">
-					<h3 class="gains-people-name"><span>Dominic Coyle</span> @ITdominiccoyle</h3>
-					<p class="gains-people-title">Deputy Business Editor, Irish Times</p>
+					<h3 class="gain-title"><span class="gain-number">191</span> <span class="gain-name">Dominic Coyle</span> @ITdominiccoyle</h3>
+					<p class="gain-job">Deputy Business Editor, Irish Times</p>
 				</li>
 				<li class="gains-people-item">
-					<h3 class="gains-people-name"><span>Richard Oakley</span> @roakleyst</h3>
-					<p class="gains-people-title">Editor, Richard Oakley</p>
+					<h3 class="gain-title"><span class="gain-number">143</span> <span class="gain-name">Richard Oakley</span> @roakleyst</h3>
+					<p class="gain-job">Editor, Richard Oakley</p>
 				</li>
 				<li class="gains-people-item">
-					<h3 class="gains-people-name"><span>Ken Early</span> @kenearlys</h3>
-					<p class="gains-people-title">Columnist/Presenter, Irish Times/RTE</p>
+					<h3 class="gain-title"><span class="gain-number">131</span> <span class="gain-name">Ken Early</span> @kenearlys</h3>
+					<p class="gain-job">Columnist/Presenter, Irish Times/RTE</p>
 				</li>
 				<li class="gains-people-item">
-					<h3 class="gains-people-name"><span>Caitriona Perry</span> @caitrionaperry</h3>
-					<p class="gains-people-title">US Correspondent, RTE</p>
+					<h3 class="gain-title"><span class="gain-number">117</span> <span class="gain-name">Caitriona Perry</span> @caitrionaperry</h3>
+					<p class="gain-job">US Correspondent, RTE</p>
 				</li>
 			</ol>
 		</div>
--- themes/murray-tweet-index/includes/parameters.php
+++ themes/murray-tweet-index/includes/parameters.php
@@ -3,6 +3,18 @@
 	<div class="params-wrapper">
 		<h2 class="section-title">6 Parameters</h2>
 		<p class="section-intro">Top ranking journalists across each of the 6 parameters measured:</p>
+		<ul class="top-params">
+			<li class="top-params-item">
+				<h3>Most Followers</h3>
+				<div class="params-image-wrapper">
+					<img src="http://twitter.com/sportsdes/profile_image?size=bigger" alt="" class="top-params-image">
+				</div>
+				<h4>David McWilliams</h4>
+				<p>Columnist, Irish Independent, SB Post</p>
+				<div>113,474</div>
+				<a href="#">View top 5</a>
+			</li>
+		</ul>
 	</div>
 	
 </section>
