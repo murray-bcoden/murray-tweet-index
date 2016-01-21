ec1d46d8847fddf6df33d380c4ba75c590f06725
Peter Barr <peter.barr@murraygroup.ie>
2016-01-21 17:36:10 +0000

    Gains desktop/tablet added

--- themes/murray-tweet-index/includes/biggest-gains.php
+++ themes/murray-tweet-index/includes/biggest-gains.php
@@ -3,6 +3,31 @@
 	<div class="gains-wrapper">
 		<h2 class="section-title">Biggest Gains</h2>
 		<p class="section-intro">vs 2014</p>
+		<div class="gains-info-wrapper">
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/images/gains.svg" alt="Positions gained infographic" class="gains-info">
+			<ol class="gains-people">
+				<li class="gains-people-item">
+					<h3 class="gains-people-name"><span>Carol Hunt</span> @carolmhunt</h3>
+					<p class="gains-people-title">Columnist, Sunday Independent</p>
+				</li>
+				<li class="gains-people-item">
+					<h3 class="gains-people-name"><span>Dominic Coyle</span> @ITdominiccoyle</h3>
+					<p class="gains-people-title">Deputy Business Editor, Irish Times</p>
+				</li>
+				<li class="gains-people-item">
+					<h3 class="gains-people-name"><span>Richard Oakley</span> @roakleyst</h3>
+					<p class="gains-people-title">Editor, Richard Oakley</p>
+				</li>
+				<li class="gains-people-item">
+					<h3 class="gains-people-name"><span>Ken Early</span> @kenearlys</h3>
+					<p class="gains-people-title">Columnist/Presenter, Irish Times/RTE</p>
+				</li>
+				<li class="gains-people-item">
+					<h3 class="gains-people-name"><span>Caitriona Perry</span> @caitrionaperry</h3>
+					<p class="gains-people-title">US Correspondent, RTE</p>
+				</li>
+			</ol>
+		</div>
 	</div>
 	
 </section>
