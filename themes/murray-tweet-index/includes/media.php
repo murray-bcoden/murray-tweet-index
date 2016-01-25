460552e7bab86a6d29f33cca81a343458491c47e
Peter Barr <peter.barr@murraygroup.ie>
2016-01-25 12:37:32 +0000

    Media RWD

--- themes/murray-tweet-index/includes/media.php
+++ themes/murray-tweet-index/includes/media.php
@@ -19,52 +19,52 @@
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Irish Times</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 85.7%"></span></div>
 						<div class="most-num">18</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Indepenedent News &amp; Media</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 71.40%"></span></div>
 						<div class="most-num">15</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Newstalk</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 38.1%"></span></div>
 						<div class="most-num">8</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">SB Post</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 19%"></span></div>
 						<div class="most-num">4</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Sunday Times</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 19%"></span></div>
 						<div class="most-num">4</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Irish Examiner</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 14.2%"></span></div>
 						<div class="most-num">3</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">journal.ie</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 9.5%"></span></div>
 						<div class="most-num">2</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">TV 3</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 9.5%"></span></div>
 						<div class="most-num">2</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">Today FM</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 9.5%"></span></div>
 						<div class="most-num">2</div>
 					</li>
 					<li class="most-list-item">
 						<div class="most-outlet">UTV Ireland</div>
-						<div class="graph-bar most-bar"><span></span></div>
+						<div class="graph-bar most-bar"><span style="width: 4.8%"></span></div>
 						<div class="most-num">1</div>
 					</li>
 				</ol>
