240b084efb53ab5fd93ad2a06f875b568d56832a
Damien McDonnell <damien@refresh.ie>
2016-01-25 10:32:59 +0000

    Added selectric dropdown, slick carousel and styled both

--- themes/murray-tweet-index/assets/js/main.js
+++ themes/murray-tweet-index/assets/js/main.js
@@ -2,6 +2,8 @@
 // @codekit-prepend "vendor/mmenu/jquery.mmenu.min.js"; 
 // @codekit-prepend "vendor/respond.min.js";
 // @codekit-prepend "vendor/zurb/responsive-tables.js";
+// @codekit-prepend "vendor/selectric/jquery.selectric.min.js";
+// @codekit-prepend "vendor/slick-carousel/slick.min.js";
 // @codekit-prepend "vendor/plugins.js";
 // @codekit-prepend "vendor/outdatedbrowser/outdatedbrowser.js";
 
@@ -13,4 +15,68 @@ jQuery(document).ready(function($){
 		// configuration
 		clone: true // clones the menu to create a separate off canvas one to help with bespoke styling of menus.
     });
+
+
+	// Initialise Selectric Dropdown and Slick Carousel
+	$('#categories').selectric();
+  
+	$('.responsive').slick({
+		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 5,
+				slidesToScroll: 1,
+			}
+		}, {
+			breakpoint: 800,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 2,
+				infinite: true,
+			}
+		}, {
+			breakpoint: 600,
+			settings: {
+			slidesToShow: 3,
+			slidesToScroll: 2,
+			infinite: true,
+		}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		}, {
+			breakpoint: 320,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+			}
+		}]
+	});
+
+	var currentIndex = 0;
+	$('#categories').on('change', function() {
+	currentIndex = $(this).prop('selectedIndex');
+	var currentSlide = $('.responsive').slick('slickCurrentSlide');
+	$('.responsive').slick('slickGoTo', parseInt(currentIndex));
+	});
+
+
+	$('.category .show-more-link').on('click', function() {
+	$(this).next().css('display', 'block');
+	});
+
+
+	$('.category .close').on('click', function() {
+	$(this).parent().css('display', 'none');
+	});
+
 });
--- themes/murray-tweet-index/includes/category-winners.php
+++ themes/murray-tweet-index/includes/category-winners.php
@@ -3,6 +3,371 @@
 	<div class="categories-wrapper">
 		<h2 class="section-title">Category Winners</h2>
 		<p class="section-intro">In addition to the overall rankings, the research ranked journalists’ influence on Twitter in a range of different categories, category winners included:</p>
+
+		<p class="categories-cta">Navigate to a Category</p>
+
+		<select name="Categories" id="categories">
+			<option value="News">News</option>
+			<option value="Business">Business</option>
+			<option value="Sport">Sport</option>
+			<option value="Politics">Politics</option>
+			<option value="Broadcast Presenter">Broadcast Presenter</option>
+			<option value="Consumer/Personal Finance">Consumer/Personal Finance</option>
+			<option value="Agriculture/Farming">Agriculture/Farming</option>
+			<option value="Technology">Technology</option>
+			<option value="Entertainment">Entertainment</option>
+			<option value="Columnist">Columnist</option>
+			<option value="Health">Health</option>
+			<option value="Food &amp; Drink">Food &amp; Drink</option>
+			<option value="Property">Property</option>
+			<option value="Fashion">Fashion</option>
+			<option value="Arts">Arts</option>
+			<option value="Editorial">Editorial</option>
+			<option value="Beauty">Beauty</option>
+		</select>
+
+		<div class="slider responsive" id="carousel">
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/SiCarswell/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+				<div class="name">Simon Carswell</div>
+				<div class="job-title">US Correspondent, Irish Times</div>
+			</div>
+			<div class="category-title">News</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-news"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/TomLyonsBiz/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Tom Lyons</div>
+			    <div class="job-title">Business Editor, Sunday Business Post</div>
+			</div>
+		    <div class="category-title">Business</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-business"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/sportsdes/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Des Cahill</div>
+			    <div class="job-title">Sports Presenter, RTE</div>
+			</div>
+		    <div class="category-title">Sport</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-sport"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/gavreilly/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Gavin Reilly</div>
+			    <div class="job-title">Political Correspondent, Today FM</div>
+			</div>
+		    <div class="category-title">Polictics</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-politics"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/ghook/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">George Hook</div>
+			    <div class="job-title">Presenter, Newstalk</div>
+			</div>
+		    <div class="category-title">Broadcast Presenter</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-broadcast"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/conor_pope/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Conor Pope</div>
+			    <div class="job-title">Consumer Affairs Correspondent, Irish Times</div>
+			</div>
+		    <div class="category-title">Consumer/Personal Finance</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-finance"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/campbellsuz/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Suzanne Campbell</div>
+			    <div class="job-title">Food and Farming journalist</div>
+			</div>
+		    <div class="category-title">Agriculture/Farming</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-agri-farming"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/adrianweckler/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Adrian Weckler</div>
+			    <div class="job-title">Technology Editor, Independent Newspapers</div>
+			</div>
+		    <div class="category-title">Technology</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-technology"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/LisaCannonXpose/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Lisa Cannon </div>
+			    <div class="job-title">Presenter, TV3</div>
+			</div>
+		    <div class="category-title">Entertainment</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-entertainment"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/unamullally/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Una Mullally</div>
+			    <div class="job-title">Columnist, Irish Times</div>
+			</div>
+		    <div class="category-title">Columnist</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-columnist"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/fergalbowers/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Fergal Bowers</div>
+			    <div class="job-title">Health Correspondent, RTE</div>
+			</div>
+		    <div class="category-title">Health</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-health"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/gnelis/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Gillian Nelis</div>
+			    <div class="job-title">Managing Editor, Sunday Business Post</div>
+			</div>
+		    <div class="category-title">Food &amp; Drink</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-food"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/berniceharrison/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Bernice Harrison</div>
+			    <div class="job-title">Property Contributor, Irish Times</div>
+			</div>
+		    <div class="category-title">Property</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-property"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/BrendanCourtney/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Brendan Courtney</div>
+			    <div class="job-title">Broadcaster, RTE</div>
+			</div>
+		    <div class="category-title">Fashion</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-fashion"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/rositaboland/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Rosita Boland</div>
+			    <div class="job-title">Arts correspondent, Irish Times</div>
+			</div>
+		    <div class="category-title">Arts</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-arts"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/davidcochrane/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">David Cochrane</div>
+			    <div class="job-title">Communities Editor, Irish Times</div>
+			</div>
+		    <div class="category-title">Editorial</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-editorial"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		  <div class="category">
+		  	<div class="category-image-wrapper">
+		  		<img class="twitter-profile" src="https://twitter.com/lovelygirlybits/profile_image?size=bigger" alt="Twitter Profile Image">
+		  	</div>
+		  	<div class="category-winner">
+			    <div class="name">Karen Constantine</div>
+			    <div class="job-title">Blogger, Lovely Girlie Bits</div>
+			</div>
+		    <div class="category-title">Beauty</div>
+		    <div class="icon-wrapper">
+		    	<div class="icon icon-beauty"></div>
+		        <a href="#" class="show-more-link">View Top 5</a>
+		    </div>
+
+		    <div class="category-top-5">
+		      <p>More info here...</p>
+		      <button class="close">X</button>
+		    </div>
+		  </div>
+		  
+		</div>
 	</div>
 	
 </section>
