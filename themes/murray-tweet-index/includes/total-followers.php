5916d6d72c7b6671c92e07cf0f6ebd0e53c7fd25
Peter Barr <peter.barr@murraygroup.ie>
2016-01-25 11:27:37 +0000

    Followers RWD

--- themes/murray-tweet-index/includes/total-followers.php
+++ themes/murray-tweet-index/includes/total-followers.php
@@ -3,7 +3,14 @@
 	<div class="followers-wrapper">
 		<h2 class="section-title">Total Followers</h2>
 		<p class="section-intro">In addition to the overall rankings, the research ranked journalists’ influence on Twitter in a range of different categories, category winners included:</p>
-		<img src="<?php echo get_template_directory_uri(); ?>/assets/images/graph-test.svg"">
 	</div>
-	
+	<div class="followers-image-wrapper">
+		<div class="content"></div>
+	</div>
+	<div class="followers-quote-wrapper">
+		<blockquote class="followers-quote">
+			<p>This research shows us the journalists who are most successfully reaching out on Twitter and engaging with their readers and listeners.</p>
+		</blockquote>
+	</div>
 </section>
+
