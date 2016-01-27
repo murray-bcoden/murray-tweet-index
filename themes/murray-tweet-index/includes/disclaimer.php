a599a29509f43fba2bc9de0d5d4ae414b931964a
Damien McDonnell <damien@refresh.ie>
2016-01-27 12:19:02 +0000

    Updated disclaimer content and responsive styling

--- themes/murray-tweet-index/includes/disclaimer.php
+++ themes/murray-tweet-index/includes/disclaimer.php
@@ -1,25 +1,52 @@
-<div id="disclaimer" class="remodal disclaimer" data-remodal-id="modal">
+<div id="disclaimer" class="remodal disclaimer" data-remodal-id="disclaimer">
 	<div class="disclaimer-wrapper">
-		<a href="#" class="disclaimer-close remodal-close"></a>
 
-    	<h1 class="disclaimer-title">Murray Tweet Index Disclaimer</h1>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci repellendus et veritatis, molestiae delectus! Accusantium eveniet repudiandae quas odio perspiciatis quaerat quasi, nulla assumenda odit quisquam recusandae omnis suscipit reprehenderit?</p>
-		<p>Nostrum libero veritatis, amet beatae sed nulla expedita mollitia. Libero repudiandae dignissimos, molestiae eos dicta, dolor blanditiis consequuntur excepturi, eveniet officia deleniti voluptatem sunt culpa nulla quos dolorum harum doloremque.</p>
-		<p>Numquam nesciunt, consequuntur quam molestiae ea, animi a provident molestias nulla nisi rerum! Dolorum laboriosam, aut corporis fugiat hic tempore saepe mollitia ipsum dignissimos consequatur nostrum deserunt architecto numquam! Itaque.</p>
-		<p>Tenetur dolorem minima qui delectus, distinctio, autem magnam itaque temporibus repellat possimus, natus? Nemo, ut, animi? At, similique, non quas dignissimos nostrum quibusdam natus neque obcaecati, perspiciatis, qui minima atque!</p>
-		<p>Mollitia itaque tempora nihil, suscipit architecto, culpa quod officiis voluptatum reprehenderit quibusdam vel laudantium vitae consectetur neque sed, a nostrum et minus. Eveniet, rerum autem nam voluptate a quisquam quidem.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci repellendus et veritatis, molestiae delectus! Accusantium eveniet repudiandae quas odio perspiciatis quaerat quasi, nulla assumenda odit quisquam recusandae omnis suscipit reprehenderit?</p>
-		<p>Nostrum libero veritatis, amet beatae sed nulla expedita mollitia. Libero repudiandae dignissimos, molestiae eos dicta, dolor blanditiis consequuntur excepturi, eveniet officia deleniti voluptatem sunt culpa nulla quos dolorum harum doloremque.</p>
-		<p>Numquam nesciunt, consequuntur quam molestiae ea, animi a provident molestias nulla nisi rerum! Dolorum laboriosam, aut corporis fugiat hic tempore saepe mollitia ipsum dignissimos consequatur nostrum deserunt architecto numquam! Itaque.</p>
-		<p>Tenetur dolorem minima qui delectus, distinctio, autem magnam itaque temporibus repellat possimus, natus? Nemo, ut, animi? At, similique, non quas dignissimos nostrum quibusdam natus neque obcaecati, perspiciatis, qui minima atque!</p>
-		<p>Mollitia itaque tempora nihil, suscipit architecto, culpa quod officiis voluptatum reprehenderit quibusdam vel laudantium vitae consectetur neque sed, a nostrum et minus. Eveniet, rerum autem nam voluptate a quisquam quidem.</p>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci repellendus et veritatis, molestiae delectus! Accusantium eveniet repudiandae quas odio perspiciatis quaerat quasi, nulla assumenda odit quisquam recusandae omnis suscipit reprehenderit?</p>
-		<p>Nostrum libero veritatis, amet beatae sed nulla expedita mollitia. Libero repudiandae dignissimos, molestiae eos dicta, dolor blanditiis consequuntur excepturi, eveniet officia deleniti voluptatem sunt culpa nulla quos dolorum harum doloremque.</p>
-		<p>Numquam nesciunt, consequuntur quam molestiae ea, animi a provident molestias nulla nisi rerum! Dolorum laboriosam, aut corporis fugiat hic tempore saepe mollitia ipsum dignissimos consequatur nostrum deserunt architecto numquam! Itaque.</p>
-		<p>Tenetur dolorem minima qui delectus, distinctio, autem magnam itaque temporibus repellat possimus, natus? Nemo, ut, animi? At, similique, non quas dignissimos nostrum quibusdam natus neque obcaecati, perspiciatis, qui minima atque!</p>
-		<p>Mollitia itaque tempora nihil, suscipit architecto, culpa quod officiis voluptatum reprehenderit quibusdam vel laudantium vitae consectetur neque sed, a nostrum et minus. Eveniet, rerum autem nam voluptate a quisquam quidem.</p>
+		<div class="disclaimer-header">
+	    	<h1 class="disclaimer-title">Privacy Policy &amp; Disclaimer</h1>
+	    	<a href="#" class="disclaimer-close remodal-close"></a>
+	    </div>
+
+		<div class="disclaimer-content">
+			<ol>
+				<li>
+					<span class="disclaimer-point-title">Site Ownership, Administration &amp; Contact Information</span>
+					<p>This website www.murraytweetindex.ie (the Site) is the property of and is administered &amp; controlled by:<br />
+					Murray Consultants Limited<br />
+					40 Baggot Street Lower<br />
+					Dublin 2</p>
+
+					<p>The primary point of contact in relation to this website is <a href="mailto:tweetindex@murraygroup.ie">tweetindex@murraygroup.ie</a></p>
+				</li>
+
+				<li>
+					<span class="disclaimer-point-title">Data Storage</span>
+					<p>There is no personal information stored about individual users of the Site.</p>
+					<p>Whenever you visit or interact with the Site, we may use technologies that collect information about how the Site is accessed and used (“Usage Information”):<br />
+					Usage Information may include browser type, device type, operating system, length of user sessions, the page served, the time, the preceding page views. The Site Usage Information is used for administration and site analytics purposes only.</p>
+					<p>We may collect and use your IP address to help diagnose problems with our server, to administer our website and to help improve the quality of service available on this site.  Your IP address is used to gather broad demographic information.  We do not link IP addresses to anything personally identifiable. This means that a user’s session will be tracked, but the user will be anonymous.</p>
+				</li>
+
+				<li>
+					<span class="disclaimer-point-title">Cookies Policy</span>
+					<p>The technologies used on the Site to collect Usage Information, include but are not limited to cookies (data files placed on a Device when it is used to visit the Site). We use cookies to store visitor preferences and record session information. To learn how you may be able to reduce the number of cookies you receive from us, or delete cookies that have already been installed in your browser’s cookie folder, please refer to your browser’s help menu or other instructions related to your browser. If you do disable or opt out of receiving cookies, we will honour your request, however please be aware that some features and services on our Site may not work properly.</p>
+				</li>
+
+				<li>
+					<span class="disclaimer-point-title">Data used in the compilation of this Data</span>
+					<p>The single purpose of this website is to deliver an analysis of Irish Media Journalists as a measure of their possible influence on Twitter.</p>
+					<p>The data used in this research was compiled from publicly available information on Twitter profiles using Twitonomy analytics (www.twitonomy.com). Data was collected for the Twitter profiles during the period from 1st January 2015 until 30th September 2015. The analysis was based on the previous 3,200 tweets that the user had made (or if the user had yet to make 3,200 tweets then all their tweets to date). The information was gathered and processed in good faith and based on our assessment of the relevant journalists who should be considered.  Any questions, comments or suggestions for improvement or inclusion in next year’s index should be sent to <a href="mailto:tweetindex@murraygroup.ie">tweetindex@murraygroup.ie</a></p>
+					<p>Any photographic images used for each Twitter Account profiled on the Site are the publicly available profile pictures uploaded to their Twitter Account and fed into the Site via a live feed from the Twitter API and have in no way been altered by the owners of the Site.</p>
+				</li>
+
+				<li>
+					<span class="disclaimer-point-title">Requests for Data Removal</span>
+					<p>If you are a journalist featured on this website and for some reason you wish to have your details removed, please confirm to us in writing by emailing us at <a href="mailto:tweetindex@murraygroup.ie">tweetindex@murraygroup.ie</a> or by regular post to the address of the owners of the site above. We will endeavour to complete any requests from any featured journalist to remove their details within 2 working days of receipt of the request (subject to confirmation of the identity of the person making the request).</p>
+				</li>
+			</ol>
+		</div>
+
+		<button data-remodal-action="confirm" class="disclaimer-ok remodal-confirm">OK</button>
+
 	</div>
-
-	<button data-remodal-action="confirm" class="disclaimer-ok remodal-confirm">OK</button>
 	
 </div>
