dbd6c88e97fdf8d121bd9e54022f690a3f51c62c
Peter Barr <peter.barr@murraygroup.ie>
2016-01-27 16:57:59 +0000

    Footer update

--- themes/murray-tweet-index/footer.php
+++ themes/murray-tweet-index/footer.php
@@ -6,11 +6,11 @@
 
 						<h2 class="footer-title">Methodology</h2>
 
-						<div class="methodology-content">
+						<!-- <div class="methodology-content"> -->
 
-							<div class="method-intro">
+							<!-- <div class="method-intro"> -->
 								<p>The study was based on the analysis of the Twitter handles of over 300 journalists carried out in September and October 2015. Using the analysis tool Twitonomy, we gathered the following data for each of the journalists:</p>
-							</div>
+							<!-- </div> -->
 							
 							<ol class="method-list">
 								<li>Total Number of Followers: <span>30%</span></li>
@@ -21,16 +21,31 @@
 								<li>Tweets per Day: <span>10%</span></li>
 							</ol>
 
+							<p>The data was based on the previous 3,200 tweets that the user had made (or if the user had yet to make 3,200 tweets then all their tweets to date). To calculate the overall rankings we attached a weighting to the users’ ranking in each of the individual parameters. The percentages shown above indicate the weighting each parameter was given in the overall score. The factors that influence the overall ranking are:</p>
+
+							<ul>
+								<li>A person’s absolute popularity on Twitter (as measured by number of followers).</li>
+								<li>The quality of engagement the user has with followers (as measured by the absolute number of  retweets and favourites, and the percentage of these in the tweets analysed).</li>
+								<li>The level of activity on Twitter (as measured by Tweets per day).</li>
+							</ul>
+
+							<p>Journalists were restricted to being considered in only one specialist category even if they might have been ranked in more than one. For example George Hook could have been considered in the sports category, but was restricted to what we viewed as his primary role as broadcast presenter.</p>
+						<!-- </div> -->
+
+					</div>
+
+					<div class="deets">
+						
+						<div class="contact">
+							<h2 class="footer-title">Contact</h2>
+							<p>For comments or suggestions on the #murraytweetindex, or to suggest a name that was not included and should have been, please email us at: <a href="mailto:<?php echo antispambot( 'tweetindex@murraygroup.ie' ); ?>"><?php echo antispambot( 'tweetindex@murraygroup.ie' ); ?></a></p>
+							<p>Website: <a href="http://www.murrayconsult.ie" target="_blank">murrayconsult.ie</a></p>
 						</div>
 
-					</div>
-
-					<div class="contact">
-						<h2 class="footer-title">Contact</h2>
-						<p>For comments or suggestions on the #murraytweetindex, or to suggest a name that was not included and should have been, please email us at: <a href="mailto:<?php echo antispambot( 'tweetindex@murraygroup.ie' ); ?>"><?php echo antispambot( 'tweetindex@murraygroup.ie' ); ?></a></p>
-					</div>
-
-					<div class="footnote">
+						<div class="twitter-follow">
+							<h2 class="footer-title">Social</h2>
+							<a href="#" id="twitter-follow">Follow @MurrayIRL on Twitter</a>
+						</div>
 
 						<div class="credit">Designed by <a href="http://www.murraycreative.ie" target="_blank" class="mc-logo-link"><img src="<?php echo get_template_directory_uri(); ?>/assets/images/murray-creative-logo.svg" alt="Murray Creative Logo"></a></div>
 						
@@ -38,10 +53,6 @@
 
 					</div>
 
-					<div class="twitter-follow">
-						<a href="#" id="twitter-follow">Follow Us</a>
-					</div>
-				
 				</div>
 
 			</footer>
