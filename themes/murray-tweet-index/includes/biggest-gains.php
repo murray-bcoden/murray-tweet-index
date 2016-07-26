b309c6f13acd934fcc4ccc0741d6921aeda38e30
Peter Barr <peter.barr@murraygroup.ie>
2016-07-26 10:24:41 +0100

    Gains self closing elements changed for gains test

--- themes/murray-tweet-index/includes/biggest-gains.php
+++ themes/murray-tweet-index/includes/biggest-gains.php
@@ -7,38 +7,38 @@
 			<svg class="circ-graph-svg content" id="circ-graph" viewBox="0 0 750 500">
 			    <defs>
 			      <filter id="goo">
-			      	<feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" />
-     				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
-     				<feBlend in="SourceGraphic" in2="goo" />
+			      	<feGaussianBlur in="SourceGraphic" stdDeviation="6" result="blur" ></feGaussianBlur>
+     				<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" ></feColorMatrix>
+     				<feBlend in="SourceGraphic" in2="goo" ></feBlend>
 			      </filter> 
 			    </defs>
 			    <g id="base-lines" opacity="0.18" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="25">
-			      <path d="M430.5,250A55.5,55.5,0,1,1,375,194.5" />
-			      <path d="M470.5,250A95.5,95.5,0,1,1,375,154.5" />
-			      <path d="M510.5,250A135.5,135.5,0,1,1,375,114.5" />
-			      <path d="M550.5,250A175.5,175.5,0,1,1,375,74.5" />
-			      <path d="M590.5,250C590.5,369,494,465.5,375,465.5S159.5,369,159.5,250,256,34.5,375,34.5" />
+			      <path d="M430.5,250A55.5,55.5,0,1,1,375,194.5" ></path>
+			      <path d="M470.5,250A95.5,95.5,0,1,1,375,154.5" ></path>
+			      <path d="M510.5,250A135.5,135.5,0,1,1,375,114.5" ></path>
+			      <path d="M550.5,250A175.5,175.5,0,1,1,375,74.5" ></path>
+			      <path d="M590.5,250C590.5,369,494,465.5,375,465.5S159.5,369,159.5,250,256,34.5,375,34.5" ></path>
 			    </g>
 			    <g id="mid-lines" opacity="0.44" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="4">
-			      <path d="M445,250a70,70,0,1,1-70-70" />
-			      <path d="M485,250A110,110,0,1,1,375,140" />
-			      <path d="M525,250A150,150,0,1,1,375,100" />
-			      <path d="M565,250c0,104.93-85.07,190-190,190S185,354.93,185,250,270.07,60,375,60" />
-			      <path d="M605,250c0,127-103,230-230,230S145,377,145,250,248,20,375,20" />
+			      <path d="M445,250a70,70,0,1,1-70-70" ></path>
+			      <path d="M485,250A110,110,0,1,1,375,140" ></path>
+			      <path d="M525,250A150,150,0,1,1,375,100" ></path>
+			      <path d="M565,250c0,104.93-85.07,190-190,190S185,354.93,185,250,270.07,60,375,60" ></path>
+			      <path d="M605,250c0,127-103,230-230,230S145,377,145,250,248,20,375,20" ></path>
 			    </g>
 			    <g id="top-lines" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="4">
-			      <path class="line0 line" d="M375,180a70,70,0,1,0,70,70" />
-			      <path class="line1 line" d="M375,140A110,110,0,1,0,485,250" />
-			      <path class="line2 line" d="M375,100A150,150,0,1,0,525,250" />
-			      <path class="line3 line" d="M375,60c-104.93,0-190,85.07-190,190s85.07,190,190,190,190-85.07,190-190" />
-			      <path class="line4 line" d="M375,20C248,20,145,123,145,250S248,480,375,480,605,377,605,250" />
+			      <path class="line0 line" d="M375,180a70,70,0,1,0,70,70" ></path>
+			      <path class="line1 line" d="M375,140A110,110,0,1,0,485,250" ></path>
+			      <path class="line2 line" d="M375,100A150,150,0,1,0,525,250" ></path>
+			      <path class="line3 line" d="M375,60c-104.93,0-190,85.07-190,190s85.07,190,190,190,190-85.07,190-190" ></path>
+			      <path class="line4 line" d="M375,20C248,20,145,123,145,250S248,480,375,480,605,377,605,250" ></path>
 			    </g>
 			    <g id="horz-lines" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="4">
-			      <line x1="375" y1="180" x2="456" y2="180" />
-			      <line x1="375" y1="140" x2="456" y2="140" />
-			      <line x1="375" y1="100" x2="456" y2="100" />
-			      <line x1="375" y1="60" x2="456" y2="60" />
-			      <line x1="375" y1="20" x2="456" y2="20" />
+			      <line x1="375" y1="180" x2="456" y2="180" ></line>
+			      <line x1="375" y1="140" x2="456" y2="140" ></line>
+			      <line x1="375" y1="100" x2="456" y2="100" ></line>
+			      <line x1="375" y1="60" x2="456" y2="60" ></line>
+			      <line x1="375" y1="20" x2="456" y2="20" ></line>
 			    </g>
 			    <g id="people">
 			      <g class="person">
@@ -69,13 +69,13 @@
 			    </g>
 			    <g class="goo" filter="url(#goo)">
 			      <g id="circles" fill="#fff" >
-			        <circle id="circ4" class="circ4 circ" cx="375" cy="20" r="15" />
-			        <circle class="circ3 circ" cx="375" cy="60" r="15" />
-			        <circle class="circ2 circ" cx="375" cy="100" r="15" />
-			        <circle class="circ1 circ" cx="375" cy="140" r="15" />
-			        <circle class="circ0 circ" cx="375" cy="180" r="15" />
+			        <circle id="circ4" class="circ4 circ" cx="375" cy="20" r="15" ></circle>
+			        <circle class="circ3 circ" cx="375" cy="60" r="15" ></circle>
+			        <circle class="circ2 circ" cx="375" cy="100" r="15" ></circle>
+			        <circle class="circ1 circ" cx="375" cy="140" r="15" ></circle>
+			        <circle class="circ0 circ" cx="375" cy="180" r="15" ></circle>
 			      </g>
-			      <circle id="center-circ" cx="375" cy="250" r="30.5" fill="#fff" />
+			      <circle id="center-circ" cx="375" cy="250" r="30.5" fill="#fff" ></circle>
 			    </g>
 			    <text id="gain" class="sans-semi" transform="translate(356.43 255)" font-size="15" fill="#7b5ba3">GAIN</text>
 				<text class="value"></text>
