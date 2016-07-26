bf99c80df439faaf5b1f99ad13fb9d279a14c753
Peter Barr <peter.barr@murraygroup.ie>
2016-07-26 10:38:34 +0100

    Test for gains trigger element change

--- themes/murray-tweet-index/assets/js/main.js
+++ themes/murray-tweet-index/assets/js/main.js
@@ -568,7 +568,7 @@ jQuery(document).ready(function($){
     ========================================================================== */
 
 	var gainsScene = new ScrollMagic.Scene({
-		triggerElement: '.circ-graph-svg', 
+		triggerElement: '.gains-info-wrapper', 
 		triggerHook: 0.8,
 		duration: 0
 	})
