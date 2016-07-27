4b9f242487cbcdb91163f294981cca7e30a99482
Peter Barr <peter.barr@murraygroup.ie>
2016-07-27 15:43:16 +0100

    Media Type Animations added

--- themes/murray-tweet-index/assets/js/main.js
+++ themes/murray-tweet-index/assets/js/main.js
@@ -512,6 +512,180 @@ jQuery(document).ready(function($){
 
 
 /*  ==========================================================================
+    Media Type Animation
+    ========================================================================== */  
+
+/*  Global variables
+    ========================================================================== */  
+
+	var percs = [22, 17, 57, 4];
+	var percsRange = []; // array of percentage ranges for the arcs to animate from to
+	var percsEnd = []; // array of percentages for the divider lines to animate to
+	var markersAngles = []; // array of percentages for markers to be positioned mid arcs
+	var $sLine = $('.mt-line'); // starting line
+	var $mtText = $('.mt-text'); // media type text
+	var $ring = $('.mt-ring'); // each percentage arc
+	var $percLine = $('.mt-perc-line'); // percentage divider lines
+	var $mtBase2 = $('.mt-base-2'); // thick white ring
+	var $mtTrack = $('.mt-base-2-track'); // thick ring track
+	var $mtMarker =$('.mt-marker'); // marker lines
+
+/*  Init media type items
+    ========================================================================== */  
+
+	TweenMax.set('svg', {
+		visibility: 'visible'
+	});
+
+	TweenMax.set($sLine, {
+		drawSVG: '0px 2px'
+	});
+
+	TweenMax.set($percLine, {
+		transformOrigin: '1px 200px',
+		opacity: 0
+	});
+
+	TweenMax.set($mtBase2, {
+		transformOrigin: '50% 50%',
+		rotation: -90,
+		drawSVG: '0% 0%'
+	});
+
+	TweenMax.set($mtTrack, {
+		transformOrigin: '50% 50%',
+		drawSVG: '0% 0%'
+	});
+
+	TweenMax.set($mtMarker, {
+		transformOrigin: '1px 277px',
+		drawSVG: '0% 0%'
+	});
+
+
+/*  Set each ring to its starting position and populate arrays
+    ========================================================================== */  
+
+	var addPercs = 0;
+	$ring.each( function(i, val) {
+		percsEnd.push(addPercs);
+
+		var tempStart = addPercs;
+		// set the starting positions
+		TweenMax.set('.mt-ring-'+i, {
+			drawSVG: addPercs + '% ' + addPercs + '%'
+		});
+		// get the ending positions and set the drawSVG ranges in an array
+		addPercs = addPercs + percs[i];
+		var rangeString = tempStart + '% ' + addPercs + '%';
+		percsRange.push(rangeString);
+		markersAngles.push((tempStart + (addPercs-tempStart)/2)/100*360);
+
+	}); 
+
+	percsEnd.reverse(); // reverse the array so the line travelling furthest goes first, and so on
+	markersAngles.reverse(); // reverse the array so the line travelling furthest goes first, and so on
+
+
+/*  Media type timeline animation
+	========================================================================== */  
+
+	var mtl = new TimelineMax();
+		// animate the base line
+		mtl.to($sLine, 0.4, {
+			drawSVG: '0px 200px',
+			ease: Power3.easeIn,
+			delay: 0.3
+		})
+		.to($sLine, 0.3, {
+			drawSVG: '200px 150px',
+			ease: Power3.easeOut
+		})
+		.set($sLine, {
+			opacity: 0
+		})
+		.set($percLine, {
+			opacity: 1
+		})
+		// animate the title text
+		.from($mtText, 0.7, { 
+			y: 50, 
+			autoAlpha: 0, 
+			ease: Elastic.easeOut.config(1.1, 0.3) 
+		}, "-=0.25")
+		// animate the base circle
+		.to($mtBase2, 1.4, {
+			drawSVG: '0% 100%',
+			ease: Power4.easeInOut
+		}, "-=0.8")
+		// animate the base circle track
+		.to($mtTrack, 1, {
+			drawSVG: '0% 100%',
+			ease: Power4.easeInOut
+		}, "-=1.05")
+		// animate the perctage lines
+		.staggerTo($percLine, 1.4, {
+			cycle: {
+				rotation: function(i) {
+					return percsEnd[i]/100 * 360;
+				}
+			},
+			ease:Power4.easeInOut
+		}, 0.1, "-=1.2")
+		// animate in marker lines
+		.staggerTo($mtMarker, 0.8, {
+			cycle: {
+				rotation: function(i) {
+					return markersAngles[i];
+				}
+			},
+			drawSVG: '0% 100%',
+			ease: Power3.easeOut
+		}, 0.1, "-=0.9")
+		// animate the rings
+		.staggerTo($ring, 1.4, {
+			cycle: {
+				drawSVG: function(i) { return percsRange[i] }
+			},
+			ease: Power4.easeInOut
+		}, 0, "-=1.3")
+		.from('.mt-figures', 1, {
+			opacity: 0
+		}, "-=0.5");
+
+/*  ==========================================================================
+    Most Tweeters Bar Graphs
+    ========================================================================== */	
+
+	var $mostList = $('.most-list');
+	var $mostItem = $('.most-list-item');
+	var $mostBar = $mostItem.find('span');
+	var mostMax = Number($mostList.find('.most-list-item:first-child').find('.most-num').html().replace(/[^0-9\.]/g, '')); // remove comma from string
+	var mostCount = 0;
+	$mostItem.each(function() {
+		var $mostItem = $(this).find('.most-bar span');
+	    var mostValue = Number($(this).find('.most-num').html().replace(/[^0-9\.]/g, '')); // get each value
+	    var mostPerc = ((mostValue/mostMax)*100)+"%"; // get percentage for each value
+	    $mostItem.attr('data-most-num', mostPerc);
+	    mostCount++;
+	});
+
+	TweenMax.set($mostBar, {
+		width:0
+	});
+
+	var mostTL = new TimelineMax();
+
+		mostTL.staggerTo($mostBar, 1, {
+			cycle: {
+				width: function(i) {
+					return this.attributes[1].nodeValue;
+				}
+			},
+			ease: Power3.easeInOut
+		}, 0.1);
+
+/*  ==========================================================================
     ScrollMagic
     ========================================================================== */
 
@@ -590,7 +764,7 @@ jQuery(document).ready(function($){
 	.addTo(controller);
 
 
-/*  6 Parameters
+/*  6 Parameters Scene
     ========================================================================== */
     if($html.hasClass('no-touch')) {
 
@@ -605,6 +779,30 @@ jQuery(document).ready(function($){
 		.addTo(controller);
 
 	}
+
+/*  Media Type Scene
+    ========================================================================== */
+
+	var mediaScene = new ScrollMagic.Scene({
+		triggerElement: '.media-type-wrapper', 
+		triggerHook: 0.7,
+		duration: 0
+	})
+	.setTween(mtl)
+	// .addIndicators()
+	.addTo(controller);
+
+/*  Most Tweeters Scene
+    ========================================================================== */
+
+	var meostScene = new ScrollMagic.Scene({
+		triggerElement: '.media-type-wrapper', 
+		triggerHook: 0.7,
+		duration: 0
+	})
+	.setTween(mostTL)
+	// .addIndicators()
+	.addTo(controller);
 
 /*  Slider
     ========================================================================== */
--- themes/murray-tweet-index/includes/media.php
+++ themes/murray-tweet-index/includes/media.php
@@ -7,7 +7,41 @@
 		<div class="media-info-wrap">
 
 			<div class="media-type-wrapper">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/media-type.svg" alt="Media Type Infographic" class="media-type">
+				<div class="content">
+					<svg class="media-type-graph" viewBox="0 0 778 590">
+						<title>Media Type Graph</title>
+						<circle class="mt-base" cx="389" cy="294" r="209" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="1.01" opacity="0.3" ></circle>
+						<path class="mt-base-2" d="M559,294A170,170,0,1,1,389,124,170,170,0,0,1,559,294Z" fill="none" stroke="#fff" stroke-miterlimit="10" stroke-width="40" ></path>
+						<g class="mt-rings" fill="none" stroke-miterlimit="10" stroke-width="10" >
+							<path class="mt-base-2-track" d="M389,124A170,170,0,1,1,219,294,170,170,0,0,1,389,124Z" stroke="#eeeeee" ></path>
+							<path class="mt-ring mt-ring-0" d="M389,124A170,170,0,1,1,219,294,170,170,0,0,1,389,124Z" stroke="#00a2d1" ></path>
+							<path class="mt-ring mt-ring-1" d="M389,124A170,170,0,1,1,219,294,170,170,0,0,1,389,124Z" stroke="#0ad100" ></path>
+							<path class="mt-ring mt-ring-2" d="M389,124A170,170,0,1,1,219,294,170,170,0,0,1,389,124Z" stroke="#f67521" ></path>
+							<path class="mt-ring mt-ring-3" d="M389,124A170,170,0,1,1,219,294,170,170,0,0,1,389,124Z" stroke="#f60d1a" ></path>
+							<text class="mt-text sans-light" x="308" y="303" font-size="30" fill="#fff" >MEDIA TYPE</text>
+						</g>
+						<g class="mt-perc-lines">
+							<rect class="mt-perc-line" x="388" y="94" width="2" height="49" fill="#fff" ></rect>
+							<rect class="mt-perc-line" x="388" y="94" width="2" height="49" fill="#fff" ></rect>
+							<rect class="mt-perc-line" x="388" y="94" width="2" height="49" fill="#fff" ></rect>
+							<rect class="mt-perc-line" x="388" y="94" width="2" height="49" fill="#fff" ></rect>
+						</g>
+						<line class="mt-line" x1="389" y1="294" x2="389" y2="94" fill="#fff" stroke="#fff" stroke-miterlimit="10" stroke-width="2" ></line>
+						<g class="mt-markers" fill="none" stroke="#fff" stroke-miterlimit="10">
+							<line class="mt-marker mt-marker-0" x1="389.5" y1="107" x2="389.5" y2="17" ></line>
+							<line class="mt-marker mt-marker-1" x1="389.5" y1="107" x2="389.5" y2="17" ></line>
+							<line class="mt-marker mt-marker-2" x1="389.5" y1="107" x2="389.5" y2="17" ></line>
+							<line class="mt-marker mt-marker-3" x1="389.5" y1="107" x2="389.5" y2="17" ></line>
+						</g>
+						<g class="mt-figures">
+							<text class="sans-light" x="260" y="20" font-size="14" fill="#fff" >ONLINE - <tspan class="sans-semi">4%</tspan></text>
+							<text class="sans-light" x="578" y="78" font-size="14" fill="#fff" >RADIO - <tspan class="sans-semi">22%</tspan></text>
+							<text class="sans-light" x="664" y="398" font-size="14" fill="#fff" >TV - <tspan class="sans-semi">17%</tspan></text>
+							<text class="sans-light" x="56" y="432" font-size="14" fill="#fff" >PRINT - <tspan class="sans-semi">57%</tspan></text>
+						</g>
+					</svg>
+				</div>
+				<!-- <img class="media-type" src="<?php echo get_template_directory_uri(); ?>/assets/images/media-type.svg" alt="Media Type Infographic"> -->
 			</div>
 			<div class="most-list-wrapper">
 				<h3 class="most-list-title">Most Tweeters in Top 100</h3>
